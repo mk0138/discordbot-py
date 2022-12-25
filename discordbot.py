763595c71fb11f4cb21f0dda240e8add1984261b
mk0138 <121390929+mk0138@users.noreply.github.com>
2022-12-25 23:54:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,12 +22,11 @@ async def on_message(message):
     if message.content == f'{PREFIX}call':
         await message.channel.send("callback!")
         
-    if message.content == f'{PREFIX}안녕':
-        await message.channel.send("만나서 반가워( ´-ㅅ•)▄︻┻┳══━一")        
+       
 
 
     if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+        await message.channel.send('Hello!( ´-ㅅ•)▄︻┻┳══━一')
 
 
 try:
